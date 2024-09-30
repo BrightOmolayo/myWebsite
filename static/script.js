1ce5950fb4b1459151228fe5137f81dc6aa98abe
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-09-30 11:23:52 +0100

    updated

--- static/script.js
+++ static/script.js
@@ -6,20 +6,17 @@ function toggle () {
 
   if (hiddenDiv.style.display === 'none') {
     hiddenDiv.style.display = 'block'
-    // line1.style.transform = 'rotate(250deg)'
-    // line2.style.transform = 'rotate(120deg)'
-    // line2.style.position = 'relative'
-    // line2.style.bottom = '5px'
 
-    line1.style.transform = 'rotate(220deg)'
+    line1.style.transform = 'rotate(225deg)'
     line1.style.position = 'relative'
-    line1.style.top = '7px'
-    line2.style.transform = 'rotate(120deg)'
+    line1.style.top = '8px'
+    line2.style.transform = 'rotate(125deg)'
+    line2.style.position = 'relative'
+    line2.style.bottom = '2px'
   } else {
     hiddenDiv.style.display = 'none'
+    // dropdowndivs.style.margin = ' 10px 4px'
     line1.style.transform = 'rotate(180deg)'
     line2.style.transform = 'rotate(180deg)'
-    line2.style.position = 'relative'
-    // line2.style.botton = '-5px'
   }
 }
